d0c20ad280be8de603cfc67d7d87dfc3ce369342
Baker Alazzawi <baker.alazzawi0@gmail.com>
2024-11-12 23:09:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React, { Suspense, lazy } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import useStore from "./zustand/store.js";
 
-// Lazy-loaded components
-const Right = lazy(() => import("../src/Component/right.jsx"));
-const Number = lazy(() => import("./Pages/number.jsx"));
-const Vaild = lazy(() => import("./Pages/pagevaild/vaild.jsx"));
-const Email = lazy(() => import("./Pages/email/email.jsx"));
-const Web = lazy(() => import("./Pages/web/web.jsx"));
+import Right from "../src/Componet/right.jsx";
+import Number from "./Pages/number.jsx";
+import Vaild from "./Pages/pagevaild/vaild.jsx";
+import Email from "./Pages/email/email.jsx";
+import Web from "./Pages/web/web.jsx";
+import useStore from "./zustand/store.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Suspense} from "react";
+
+
+
+
+
 
 function App() {
   const { count } = useStore();
